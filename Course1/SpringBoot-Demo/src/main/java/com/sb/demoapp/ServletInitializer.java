40a1f9b9e767fef41d477b3c06b4af2c7aa672b2
Vijay R. Gawade <vijay.gawade96@outlook.com>
2021-12-25 14:45:49 +0530

    Deploy WAR as Deployment

---
+++ Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.sb.demoapp;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootDemoApplication.class);
+	}
+}
