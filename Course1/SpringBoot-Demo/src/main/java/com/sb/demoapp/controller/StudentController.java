9e3a28204e6e2bc1f27d2ad27f8ad6ef7705c2c7
Vijay R. Gawade <vijay.gawade96@outlook.com>
2021-12-25 13:12:06 +0530

    Change Embedded Server - Tomcat, Jetty and Undertow

--- Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/controller/StudentController.java
+++ Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/controller/StudentController.java
@@ -3,8 +3,6 @@ package com.sb.demoapp.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
