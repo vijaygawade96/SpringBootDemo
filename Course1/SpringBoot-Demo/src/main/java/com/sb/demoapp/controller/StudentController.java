993b9b98c2cb9306dbbdaacd8909dac6e4cec5d7
Vijay R. Gawade <vijay.gawade96@outlook.com>
2021-12-25 12:34:01 +0530

    Create REST API returns List as JSON Array

--- Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/controller/StudentController.java
+++ Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/controller/StudentController.java
@@ -1,5 +1,8 @@
 package com.sb.demoapp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,17 @@ public class StudentController {
 	@GetMapping(path = "/student1")
 	public Student getStud1Details() {
 		return new Student("Vijay", "Gawade");
+	}
+	
+	@GetMapping(path = "/students")
+	public List<Student> getAllStudents(){
+		List<Student> students =new ArrayList<Student>();
+		students.add(new Student("Vijay","Gawade"));
+		students.add(new Student("Prakash","Sawant"));
+		students.add(new Student("Rohit","Rawool"));
+		students.add(new Student("Amit","Bagaitkar"));
+		students.add(new Student("Varsha","Naik"));
+		
+		return students;
 	}
 }
