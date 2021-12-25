1f81645d7d816dc910bb4a5984fbbe4c3fea674e
Vijay R. Gawade <vijay.gawade96@outlook.com>
2021-12-25 12:48:15 +0530

    REST API with Request Param - @RequestParam

--- Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/controller/StudentController.java
+++ Course1/SpringBoot-Demo/src/main/java/com/sb/demoapp/controller/StudentController.java
@@ -7,6 +7,7 @@ import javax.websocket.server.PathParam;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sb.demoapp.model.Student;
@@ -30,12 +31,21 @@ public class StudentController {
 		return students;
 	}
 	
-	@GetMapping(path = "/student/{id}")
+	@GetMapping(path = "/students/{id}")
 	public Student getStudentDetails(@PathVariable("id") int id) {
 		if(id == 1)
 			return new Student("Vijay", "Gawade");
 		else if(id==2)
 			return new Student("Varsha", "Naik");
+		return null;
+	}
+	
+	@GetMapping(path = "/students2")
+	public Student getStudentDetailsRQParam(@RequestParam(name="id") int id) {
+		if(id == 1)
+			return new Student("Prakash", "Sawant");
+		else if(id==2)
+			return new Student("Unnati", "More");
 		return null;
 	}
 }
